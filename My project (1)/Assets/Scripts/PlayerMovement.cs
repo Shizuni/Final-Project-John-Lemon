16bff6aace6010be5af4ca6ce12b7c0e4f120fac
Shizuni <TheAmazingViolet@yahoo.com>
2023-12-06 12:15:59 -0500

    started major change
    spider to chase john lemon

--- My project (1)/Assets/Scripts/PlayerMovement.cs
+++ My project (1)/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float turnSpeed = 20f;
-    public float walkSpeed = 0.0001f;
-    public float sprintSpeed = 0.3f; // New variable for sprint speed
+    public float walkSpeed = 2f;
+    public float sprintSpeed = 4f; 
 
     private Animator m_Animator;
     private Rigidbody m_Rigidbody;
     private AudioSource m_AudioSource;
     private Vector3 m_Movement;
     private Quaternion m_Rotation = Quaternion.identity;
-    private bool isSprinting = false; // New variable to track sprinting state
+    private bool isSprinting = false; 
 
     private void Start()
     {
@@ -23,10 +23,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        // Check if the sprint key is held down
+        
         isSprinting = Input.GetKey(KeyCode.LeftShift);
 
-        // You can add additional checks here for other keys if needed
+        
     }
 
     private void FixedUpdate()
@@ -41,7 +41,7 @@ public class PlayerMovement : MonoBehaviour
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
 
-        // Adjust speed based on whether sprint key is held down
+        
         float speed = isSprinting ? sprintSpeed : walkSpeed;
 
         m_Animator.SetBool("IsWalking", isWalking);
@@ -61,13 +61,13 @@ public class PlayerMovement : MonoBehaviour
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
 
-        // Apply movement with adjusted speed
+        
         m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * speed * Time.fixedDeltaTime);
     }
 
     private void OnAnimatorMove()
     {
-        // Move rotation as before
+        
         m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
